d5ce048cfa6598261210aeb5bb52b41b709722a3
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-09-19 12:47:30 -0700

    Migrate to typescript and fix type errors

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,26 @@ const URLS = {
     "https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json",
 };
 
+interface GitlyError {
+  code?: number
+}
+
 /** Download repository and fallback to other branch if it doesn't use `master`. */
-async function downloadRepo(repo) {
-  let source;
+async function downloadRepo(repo:string): Promise<void> {
+  let source: string;
 
   // Fallback to main branch if master doesn't work. The default in `gitly` is `master` and most Obsidian repositories use it.
   try {
     // Try to download from `master` branch.
     source = await download(repo, { throw: true });
-  } catch (error) {
+  } catch (error: unknown) {
     // If not found, try to download from main brach. Else it's an other error.
-    if (error.code === 404) {
+    const gitlyError = error as GitlyError;
+    if (gitlyError.code === 404) {
       source = await download(`${repo}#main`, { throw: true });
     } else {
       console.error(error);
+      throw error;
     }
   }
 
@@ -30,7 +36,7 @@ async function downloadRepo(repo) {
 }
 
 /** Downloads GitHub repositories that aren't already downloaded, creating a tree structure. */
-async function downloadRepositories(repos) {
+async function downloadRepositories(repos: string[]): Promise<void> {
   const downloadBar = new cliProgress.SingleBar({
     format: "[{bar}] {percentage}% | {value}/{total} | 📂 {repo}",
     hideCursor: true,
@@ -38,28 +44,38 @@ async function downloadRepositories(repos) {
 
   downloadBar.start(repos.length, 0);
 
-  for (const repo of repos) {
-    await downloadRepo(repo).then(downloadBar.increment({ repo: repo }));
+  for await (const repo of repos) {
+    await downloadRepo(repo);
+    downloadBar.increment({ repo: repo });
   }
 
   downloadBar.stop();
 }
+
+interface Plugin {
+  id: string;
+  name: string;
+  author: string;
+  description: string;
+  repo: string;
+}
+
+type Plugins = Plugin[];
 
 /** Return list of plugin repositories on GitHub. */
 async function getPluginsRepos() {
   const spinner = ora("Fetching community-plugins.json").start();
 
   // Fetch community-plugins.json and turn it into object
-  const communityPluginsJson = await fetch(URLS.COMMUNITY_PLUGINS).then(
-    (response) => response.json()
-  );
+  const response = await fetch(URLS.COMMUNITY_PLUGINS);
+  const communityPluginsJson: Plugins = await response.json() as Plugins
 
   // Get "repo" from every plugin.
   const pluginsRepos = communityPluginsJson.map((plugin) => plugin.repo);
 
   spinner.succeed(`Found ${pluginsRepos.length} plugins!`);
 
-  // [author/pluginname, author2/pluginname3...]
+  // [author/plugin_name, author2/plugin_name3...]
   return pluginsRepos;
 }
 
